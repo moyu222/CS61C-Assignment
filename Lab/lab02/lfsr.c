4db8d20442f737169e37f791826a67b4b8c4ee36
zhangyangzonghan <zyzh050415@gmail.com>
2025-03-18 17:02:06 +0800

    "lab02 completed"

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -6,5 +6,8 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t num = *reg;
+    int bit = (num ^ (num>>2) ^ (num>>3) ^ (num>>5)) & 1;
+    *reg = ((*reg>>1) & ~(1U<<15)) | (bit<<15);
 }
 
