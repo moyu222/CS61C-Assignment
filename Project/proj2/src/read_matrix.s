dcc87d9daa379e104c0755e9c598f3c5d3749a3b
zhangyangzonghan <zyzh050415@gmail.com>
2025-04-02 16:56:17 +0800

    partb1

--- Project/proj2/src/read_matrix.s
+++ Project/proj2/src/read_matrix.s
@@ -27,16 +27,77 @@
 read_matrix:
 
     # Prologue
+    addi sp, sp, -24
+    sw, s0, 0(sp)
+    sw, s1, 4(sp)
+    sw, s2, 8(sp)
+    sw, s3, 12(sp)
+    sw, s4, 16(sp)
+    sw, s5, 20(sp)
 	
+    mv s0, a0 # the pointer of filename  
+    mv s1, a1 # the pointer to rows num
+    mv s2, a2 # the pointer to cols num 
 
+    li a2, 0
+    jal fopen
+    blt a0, x0, error_90
+    mv s3, a0 # the file descriptor
 
+    li a0, 2
+    jal malloc
+    beq a0, x0, error_88
+    mv a2, a0
+    mv s4, a2 # the address of buffer
+    mv a1, s3 
+    li a3, 2
+    jal fread
+    bne a0, a3, error_91
+    lw t0, 0(s4) # num of rows 
+    lw t1, 4(s4) # num of cols
+    mul s5, t0, t1 # total number of int
 
+    li a0, s5 
+    jal malloc
+    beq a0, x0, error_90
+    mv a2, a0
+    mv s4, a2
+    mv a1, s3 
+    mv a3, s5
+    jal fread 
+    bne a0, a3, error_91
 
+    mv a1, s3
+    jal fclose 
+    bne a0, x0, error_92
 
+    mv a0, s4
+    mv a1, s1
+    mv a2, s2
 
-
-
+ 
     # Epilogue
-
+    lw, s0, 0(sp)
+    lw, s1, 4(sp)
+    lw, s2, 8(sp)
+    lw, s3, 12(sp)
+    lw, s4, 16(sp)
+    lw, s5, 20(sp)
+    addi sp, sp, 24
 
     ret
+error_88:
+	li a1, 88
+    jal exit2
+
+error_90:
+	li a1, 90
+    jal exit2
+
+error_91:
+	li a1, 91
+    jal exit2
+    
+error_92:
+	li a1, 92
+    jal exit2
