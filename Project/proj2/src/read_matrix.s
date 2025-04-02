faf66c199f65272fea6810472e55bc0d89908374
zhangyangzonghan <zyzh050415@gmail.com>
2025-04-02 18:00:45 +0800

    proj2 completed :)

--- Project/proj2/src/read_matrix.s
+++ Project/proj2/src/read_matrix.s
@@ -27,77 +27,100 @@
 read_matrix:
 
     # Prologue
-    addi sp, sp, -24
-    sw, s0, 0(sp)
-    sw, s1, 4(sp)
-    sw, s2, 8(sp)
-    sw, s3, 12(sp)
-    sw, s4, 16(sp)
-    sw, s5, 20(sp)
+	addi sp, sp, -36
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
+    sw s4, 20(sp)
+    sw s5, 24(sp)
+    sw s6, 28(sp)
+    sw s7, 32(sp)
+    
+    mv s0, a0
+    mv s1, a1
+    mv s2, a2
 	
-    mv s0, a0 # the pointer of filename  
-    mv s1, a1 # the pointer to rows num
-    mv s2, a2 # the pointer to cols num 
-
-    li a2, 0
+    # open file
+	mv a1, s0 # filename
+	li a2, 0  # read-only
     jal fopen
-    blt a0, x0, error_90
-    mv s3, a0 # the file descriptor
+    li t0, -1
+    beq a0, t0, exit_90
+    mv s3, a0 # fd 
 
-    li a0, 2
-    jal malloc
-    beq a0, x0, error_88
-    mv a2, a0
-    mv s4, a2 # the address of buffer
-    mv a1, s3 
-    li a3, 2
-    jal fread
-    bne a0, a3, error_91
-    lw t0, 0(s4) # num of rows 
-    lw t1, 4(s4) # num of cols
-    mul s5, t0, t1 # total number of int
-
-    li a0, s5 
-    jal malloc
-    beq a0, x0, error_90
-    mv a2, a0
-    mv s4, a2
-    mv a1, s3 
-    mv a3, s5
-    jal fread 
-    bne a0, a3, error_91
-
+    # read in # of rows and columns
     mv a1, s3
-    jal fclose 
-    bne a0, x0, error_92
-
-    mv a0, s4
-    mv a1, s1
+    mv a2, s1
+    li a3, 4
+    jal fread
+    li t0, 4
+    bne t0, a0, exit_91
+   	mv a1, s3
     mv a2, s2
+    li a3, 4
+    jal fread
+    li t0, 4
+    bne t0, a0, exit_91
+    lw s1, 0(s1) # of rows
+    lw s2, 0(s2) # of columns
 
- 
+    # allocate memory for matrix
+    mul a0, s1, s2
+    slli a0, a0, 2
+	jal malloc
+    beq x0, a0, exit_88
+	mv s4, a0 # pointer to the matrix
+    
+    # read in matrix
+	mul s5, s1, s2 # total number of integers
+    li s6, 0       # i = 0
+    mv s7, s4      # pointer + offset
+loop_begin:
+	mv a1, s3
+    mv a2, s7
+    li a3, 4
+    jal fread
+    li t0, 4
+    bne t0, a0, exit_91
+    addi s6, s6, 1 # i += 1
+    beq s6, s5, loop_end
+    addi s7, s7, 4 # pointer + 4
+    j loop_begin
+loop_end:
+	# close the file
+	mv a1, s3
+    jal fclose
+    li t0, -1
+    beq t0, a0, exit_92
+	
+    mv a0, s4
     # Epilogue
-    lw, s0, 0(sp)
-    lw, s1, 4(sp)
-    lw, s2, 8(sp)
-    lw, s3, 12(sp)
-    lw, s4, 16(sp)
-    lw, s5, 20(sp)
-    addi sp, sp, 24
-
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    lw s6, 28(sp)
+    lw s7, 32(sp)
+	addi sp, sp, 36
+    
     ret
-error_88:
+exit_88:
 	li a1, 88
     jal exit2
 
-error_90:
+exit_90:
 	li a1, 90
     jal exit2
 
-error_91:
+exit_91:
 	li a1, 91
     jal exit2
     
-error_92:
+exit_92:
 	li a1, 92
     jal exit2
